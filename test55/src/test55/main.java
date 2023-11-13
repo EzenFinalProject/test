1ecc49cd19000da05f5e41ab7a45fdb5ecde39f8
kimmustard <kimustard44@gmail.com>
2023-11-13 16:43:49 +0900

    엄준식뽑기 ver0.2

--- test55/src/test55/main.java
+++ test55/src/test55/main.java
@@ -7,8 +7,8 @@ public class main {
 		System.err.println("hello world");
 		System.err.println("hello world2");
 		System.err.println("hello world3");
-		System.err.println("hello world4");
 		System.err.println("hello world5");
+		System.err.println("hello world4");
 		
 		System.out.println("랄로");
 		
