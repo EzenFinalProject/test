84a2bb7de59f80c9c6f53db33271c0933ac3b9e9
ffinky99 <ffinky99@gmail.com>
2023-11-13 16:41:45 +0900

    엄준식 뽑기

---
+++ test55/src/test55/ummJunSick.java
@@ -0,0 +1,31 @@
+package test55;
+
+import java.util.Random;
+
+public class ummJunSick {
+	
+	
+	public static void main(String[] args) {
+	
+			// 어떻게 사람 이름이 엄준식??
+		
+			for (int i = 0; i <= 2; i++) {
+				int r = (int) (Math.random()*3)+0;
+				if(r==0) {
+					System.out.println("엄");	
+					
+				}
+				if(r==1) {	
+					
+					System.out.println("준");
+				}
+				if(r==2) {	
+					
+					System.out.println("식");
+				}
+			}
+		}
+		
+	
+	
+}
